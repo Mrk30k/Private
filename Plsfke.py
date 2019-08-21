663683acf4c68fe59f31f51a0c96d86ea703784b
Mr_k30k <54361420+Mrk30k@users.noreply.github.com>
2019-08-21 21:40:01 +0700

    Add files via upload
    Hanya script mainan

---
+++ Plsfke.py
@@ -0,0 +1,126 @@
+import time
+time.sleep(3)
+
+
+print ("===================================================")
+print (" _  __          ____  _   _ _     ____    _ ")
+print ("| |/ /         |  _ \| | | | |   / ___|  / \ ")
+print ("| ' /   _____  | |_) | | | | |   \___ \ / _ \ ")
+print ("| . \  |_____| |  __/| |_| | |___ ___) / ___ \ ")
+print ("|_|\_\         |_|    \___/|_____|____/_/   \_\ ")
+print ("=================================================== ")
+print ("script by : MR_K30K ")
+print ("=================================================== ")
+print ("whatsapp  : +6281311795580 ")
+print ("=================================================== ")
+print ("grub      : ☆《ĎĚVŁÎÑ》☆ ")
+print ("=================================================== ")
+time.sleep(2)
+print ()
+print ('hargai sedikit pembuatnya,jangan asal edit aja')
+
+time.sleep(5)
+print ()
+time.sleep(1)
+print ()
+time.sleep(1)
+print ()
+time.sleep(1)
+print ("[•]hapus angka 0 paling depan")
+time.sleep(2)
+print ("[•]contoh 8131×××××××")
+time.sleep(2)
+print ()
+print ()
+nomor = input('[~]nomor anda : ')
+time.sleep(2)
+print ("[•]jumblah pulsa max 15.000 per hari")
+time.sleep(1)
+print ()
+pulsa = input('[~]pulsa      : ')
+time.sleep(3)
+print ()
+print ()
+print ('mengirim pulsa', pulsa)
+time.sleep(2)
+print ('ke nomer', nomor)
+time.sleep(3)
+print ()
+print ('! proses !')
+time.sleep(2)
+print ()
+time.sleep(1)
+
+print ('10%')
+time.sleep(1)
+print ('20%')
+time.sleep(1)
+print ('30%')
+time.sleep(1)
+print ('40%')
+time.sleep(1)
+print ('50%')
+time.sleep(1)
+print ('60%')
+time.sleep(1)
+print ('70%')
+time.sleep(1)
+print ('80%')
+time.sleep(1)
+print ('90%')
+time.sleep(1)
+print ('100%')
+time.sleep(2)
+print ()
+print ()
+print ('tunggu 5 sampai 10 detik,jika belum ada pesan silahkan ketik  yang tertera di bawah')
+time.sleep(4)
+print ()
+tanya = input('belum menerima pesan y/n : ')
+time.sleep(2)
+print ()
+print ()
+print ('berarti anda')
+print ()
+time.sleep(3)
+text = '''
+    __ __ _______   _____       ____  ____  ___    _   ____ __
+   / //_// ____/ | / /   |     / __ \/ __ \/   |  / | / / //_/
+  / ,<  / __/ /  |/ / /| |    / /_/ / /_/ / /| | /  |/ / ,<
+ / /| |/ /___/ /|  / ___ |   / ____/ _, _/ ___ |/ /|  / /| |
+/_/ |_/_____/_/ |_/_/  |_|  /_/   /_/ |_/_/  |_/_/ |_/_/ |_| '''
+print (text)
+time.sleep(3)
+print ()
+print ()
+
+text1 = '''
+===================================================================
+  _   _   _     _   _   _   _   _     _   _   _   _   _   _   _   _   _
+ / \ / \ / \   / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \ / \
+
+( c | i | e ) ( g | a | g | a | l ) ( m | e | n | g | h | a | r | a | p )
+ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/
+ ===================================================================
+'''
+print (text1)
+time.sleep(3)
+print ()
+print ()
+text2 = '''
+  _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _
+ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \
+
+( w | k | w | k | w | k | w | k | w | w | k | w | k | w | k | w | k | w | k )
+ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ '''
+print (text2)
+time.sleep(3)
+print ()
+
+print ('[°]terima kasih sudah mengikiti langkah langkahnya,wkwkwkwk.harap maklum ini hanya script mainan saja bukan script beneran jadi gak heran kalo scriptnya gak work')
+
+time.sleep(10)
+
+print ('#dapat salam dari MR_K30K')
+time.sleep(2)
+
